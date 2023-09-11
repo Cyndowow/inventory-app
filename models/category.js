8f92862eb604fffe49c4edb247a1c9309b15ae7e
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-09-11 10:26:11 +0200

    Add models

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+    name: { type: String, required: true, minLength: 1},
+    description: { type: String, required: true },
+    url: { type: String, required: true, minLength: 1}
+})
+
+CategorySchema.virtual("url").get(function () {
+    return `/category/${this._id}`
+})
+
+module.exports = mongoose.model("Category", CategorySchema)
